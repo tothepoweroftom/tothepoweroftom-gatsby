7e2797d4cf3c40063cd909a99ccbe92ffc47a9a3
Thomas Power <tom@wildsurmisemusic.com>
2019-07-01 11:51:56 +0200

    asdf

---
+++ src/components/IntroBlurb.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+
+const IntroBlurb = ( {} ) => (
+
+  <div className="columns is-multiline">
+
+          <p>{}</p>
+  </div>
+)
+
+IntroBlurb.propTypes = {
+
+}
+
+export default IntroBlurb
