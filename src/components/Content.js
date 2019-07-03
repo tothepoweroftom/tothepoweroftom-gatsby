579a902b4488020462fd57c0717fbc7c4fe864ef
Thomas Power <tom@wildsurmisemusic.com>
2019-07-03 14:07:24 +0200

    adding config to admin

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+// 
 export const HTMLContent = ({ content, className }) => (
   <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
 )
